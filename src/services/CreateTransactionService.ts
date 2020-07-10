e0c1c1f79a1095544f5ccdf8bfabea02d7f0063e
Luiz Felipe do Divino <lf.divino@gmail.com>
2020-07-10 16:29:39 -0300

    Implemented services functionalities

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,67 @@
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+
+interface Request {
+  title: string,
+  type: 'income' | 'outcome',
+  value: number,
+  category: string
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, type, value, category }: Request): Promise<Transaction> {
+    if (!title || !type || !value || !category) {
+      throw new AppError("All the fields are required!");
+    }
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw new AppError("Transaction type could only be 'income' or 'outcome'!");
+    }
+
+    if (value < 0) {
+      throw new AppError("Value needs to be positive!");
+    }
+
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const balance = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && value > balance.total) {
+      throw new AppError("You couldn't outcome a value bigger than balance's total!");
+    }
+
+    const categoryRepository = getRepository(Category);
+
+    const category_exists = await categoryRepository.findOne({ where: { title: category }});
+
+    if (!category_exists) {
+      const category_obj = categoryRepository.create({ title: category });
+      await categoryRepository.save(category_obj);
+
+      const transaction = transactionRepository.create({
+        title,
+        type,
+        value,
+        category: category_obj.id
+      });
+      await transactionRepository.save(transaction);
+
+      return (transaction);
+    } else {
+      const transaction = transactionRepository.create({
+        title,
+        type,
+        value,
+        category: category_exists.id
+      });
+      await transactionRepository.save(transaction);
+
+      return (transaction);
+    }
   }
 }
 
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,58 @@
+import fs from 'fs';
+import csvParse from 'csv-parse';
+import path from 'path';
+
+import UploadConfig from '../config/upload';
 import Transaction from '../models/Transaction';
+import { getCustomRepository } from 'typeorm';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from './CreateTransactionService';
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(csv: string): Promise<Transaction[]> {
+    const transactions_data: { title: string; type: 'income' | 'outcome'; value: number; category: string; }[] = [];
+    const transactions: Transaction[] = []
+    const createTransactionsService = new CreateTransactionService();
+
+    const readCSV = async () => {
+      const csvFilePath = path.join(UploadConfig.directory, csv);
+
+      const readCSVStream = fs.createReadStream(csvFilePath);
+
+      const parseStream = csvParse({ 
+        from_line: 2,
+        ltrim: true,
+        rtrim: true,
+      });
+      
+      const parseCSV = readCSVStream.pipe(parseStream);
+
+      parseCSV.on('data', async line => {
+        transactions_data.push({
+          title: line[0],
+          type: line[1],
+          value: line[2],
+          category: line[3]
+        });
+      });
+
+      await new Promise(resolve => {
+        parseCSV.on('end', resolve);
+      });
+    };
+
+    await readCSV();
+
+    const run = async () => {
+      for (let index = 0; index < transactions_data.length; index++) {
+        const new_transaction = await createTransactionsService.execute(transactions_data[index]);
+        transactions.push(new_transaction);
+      }  
+    };
+
+    await run();
+    
+    return (transactions);
   }
 }
 
